8d59e9cd5aa6b4937c898ae2170aef4e558c141e
Alessio Buccino <alejoe9187@gmail.com>
2018-10-23 09:18:49 +0200

    exposed old implementation of kilosort and spyking circus

--- spiketoolkit/sorters/__init__.py
+++ spiketoolkit/sorters/__init__.py
@@ -3,4 +3,7 @@ from .spyking_circus import spyking_circus
 from .ironclust import ironclust
 from .klusta import klusta
 from .kilosort import kilosort
+
+from .old import kilosort_j, spyking_circus_j
+
 from .tools import run_command_and_print_output
---
+++ spiketoolkit/sorters/old/__init__.py
@@ -0,0 +1,2 @@
+from .kilosort_j import kilosort_j
+from .spyking_circus_j import spyking_circus_j
