89e0cf1c8f1dc0ad16d83c7f184d706ff5e6a010
Alessio Buccino <alejoe9187@gmail.com>
2018-10-23 10:18:37 +0200

    removed old sorters before merging to master

--- spiketoolkit/sorters/__init__.py
+++ spiketoolkit/sorters/__init__.py
@@ -4,6 +4,4 @@ from .ironclust import ironclust
 from .klusta import klusta
 from .kilosort import kilosort
 
-from .old import kilosort_j, spyking_circus_j
-
 from .tools import run_command_and_print_output
--- spiketoolkit/sorters/old/kilosort_j/__init__.py
+++
@@ -1,1 +0,0 @@
-from .kilosort import kilosort_j
