8e073ef5d062b4e7fde71cd0b5e7e74e6ae2a296
Alessio Buccino <alejoe9187@gmail.com>
2018-12-07 19:10:49 +0100

    fixed rectify preprocess

--- spiketoolkit/preprocessing/__init__.py
+++ spiketoolkit/preprocessing/__init__.py
@@ -2,3 +2,4 @@ from .bandpass_filter import bandpass_filter
 from .whiten import whiten
 from .common_reference import common_reference
 from .resample import resample
+from .rectify import rectify
--- spiketoolkit/preprocessing/rectify.py
+++ spiketoolkit/preprocessing/rectify.py
@@ -1,54 +1,32 @@
 from spikeextractors import RecordingExtractor
 import numpy as np
-from scipy import special, signal
 
-class ResampledRecording(RecordingExtractor):
-    def __init__(self, *, recording, resample_rate):
+class RectifyRecording(RecordingExtractor):
+    def __init__(self, *, recording):
         RecordingExtractor.__init__(self)
         self._recording = recording
-        self._resample_rate = resample_rate
         self.copyChannelProperties(recording)
 
     def getSamplingFrequency(self):
-        return self._resample_rate
+        return self._recording.getSamplingFrequency()
 
     def getNumFrames(self):
-        return int(self._recording.getNumFrames() / self._recording.getSamplingFrequency() * self._resample_rate)
+        return self._recording.getNumFrames()
 
     def getTraces(self, channel_ids=None, start_frame=None, end_frame=None):
         if start_frame is None:
-            start_frame_not_sampled = 0
-            start_frame_sampled = 0
-        else:
-            start_frame_not_sampled = int(start_frame / self.getSamplingFrequency() *
-                                          self._recording.getSamplingFrequency())
-            start_frame_sampled = start_frame
+            start_frame = 0
         if end_frame is None:
-            end_frame_not_sampled = self._recording.getNumFrames()
-            end_frame_sampled = self.getNumFrames()
-        else:
-            end_frame_not_sampled = int(end_frame / self.getSamplingFrequency() * self._recording.getSamplingFrequency())
-            end_frame_sampled = end_frame
+            end_frame = self.getNumFrames()
         if channel_ids is None:
             channel_ids = self.getChannelIds()
-        traces = self._recording.getTraces(start_frame=start_frame_not_sampled,
-                                           end_frame=end_frame_not_sampled,
-                                           channel_ids=channel_ids)
-        if np.mod(self._recording.getSamplingFrequency(), self._resample_rate) == 0:
-            print('Decimate')
-            traces_resampled = signal.decimate(traces,
-                                               q=int(self._recording.getSamplingFrequency() / self._resample_rate),
-                                               axis=1)
-        else:
-            traces_resampled = signal.resample(traces, int(end_frame_sampled - start_frame_sampled), axis=1)
-        return traces_resampled
+        return np.abs(self._recording.getTraces(channel_ids=channel_ids, start_frame=start_frame, end_frame=end_frame))
 
     def getChannelIds(self):
         return self._recording.getChannelIds()
 
 
-def resample(recording, resample_rate):
-    return ResampledRecording(
-        recording=recording,
-        resample_rate=resample_rate
+def rectify(recording):
+    return RectifyRecording(
+        recording=recording
     )
