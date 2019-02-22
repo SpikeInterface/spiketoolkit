caf0af0063f0f1b7a20988772add6d9c62863e8e
Samuel Garcia <sam.garcia.die@gmail.com>
2019-02-22 12:00:29 +0100

    Multiprocessing support for launcher.

--- spiketoolkit/sorters/launcher.py
+++ spiketoolkit/sorters/launcher.py
@@ -6,25 +6,62 @@ from pathlib import Path
 
 from .sorterlist import sorter_dict, run_sorter
 
+import multiprocessing
 
 
-def run_sorters(recording_dict, sorter_list, working_folder, debug=False):
+def _run_one(arg_list):
+    # the multiprocessing python module force to have one unique tuple argument
+    rec_name, recording, sorter_name, output_folder, debug = arg_list
+    
+    os.makedirs(output_folder)
+    params = sorter_dict[sorter_name].default_params()
+    run_sorter(sorter_name, recording, output_folder=output_folder, debug=debug, **params)
+    
+    
+
+def run_sorters(sorter_list, recording_dict_or_list,  working_folder, engine=None, debug=False):
     """
     This run several sorter on several recording.
     Simple implementation will nested loops.
     
     Need to be done with multiprocessing.
+    
+    sorter_list: list of str (sorter names)
+    recording_dict_or_list: a dict (or a list) of recording
+    working_folder : str
+    
     """
     
     assert not os.path.exists(working_folder), 'working_folder already exists, please remove it'
     
+    if isinstance(recording_dict_or_list, list):
+        # in case of list
+        recording_dict = { 'recording_{}'.format(i): rec for i, rec in enumerate(recording_dict_or_list) }
+    elif isinstance(recording_dict_or_list, dict):
+        recording_dict = recording_dict_or_list
+    else:
+        raise(ValueError('bad recording dict'))
+    
+    
     working_folder = Path(working_folder)
     
+    task_list = []
     for rec_name, recording in recording_dict.items():
         for sorter_name in sorter_list:
             output_folder = working_folder / rec_name / sorter_name
-            os.makedirs(output_folder)
-            params = sorter_dict[sorter_name].default_params()
-            run_sorter(sorter_name, recording, output_folder=output_folder, debug=debug, **params)
+            task_list.append((rec_name, recording, sorter_name, output_folder, debug))
+            
+            
+
+    
+    if engine is None:
+        # simple loop
+        for arg_list in task_list:
+            _run_one(arg_list)
+    
+    elif engine == 'multiprocessing':
+        pool = multiprocessing.Pool()
+        pool.map(_run_one, task_list)
         
+    
     
--- spiketoolkit/sorters/tests/test_launcher.py
+++ spiketoolkit/sorters/tests/test_launcher.py
@@ -8,6 +8,18 @@ import spikeextractors as se
 
 
 
+def test_run_sorters_with_list():
+    rec0, _ = se.example_datasets.toy_example1(num_channels=4, duration=30)
+    rec1, _ = se.example_datasets.toy_example1(num_channels=8, duration=30)
+    
+    recording_list = [rec0, rec1]
+    sorter_list = ['tridesclous']
+    working_folder = 'test_run_sorters_list'
+    if os.path.exists(working_folder):
+        shutil.rmtree(working_folder)
+    run_sorters(sorter_list, recording_list, working_folder, debug=False)
+    
+    
 
 def test_run_sorters():
     rec0, _ = se.example_datasets.toy_example1(num_channels=4, duration=30)
@@ -15,15 +27,27 @@ def test_run_sorters():
     
     recording_dict = {'toy_tetrode' : rec0, 'toy_octotrode': rec1}
     
-    sorter_list = ['mountainsort4', 'klusta', 'tridesclous']
+    # sorter_list = ['mountainsort4', 'klusta', 'tridesclous']
+    sorter_list = ['tridesclous']
     
     working_folder = 'test_run_sorters'
     if os.path.exists(working_folder):
         shutil.rmtree(working_folder)
     
-    run_sorters(recording_dict, sorter_list, working_folder)
+    
+    # simple loop
+    run_sorters(sorter_list, recording_dict, working_folder, engine=None)
+    
+    shutil.rmtree(working_folder)
+    
+    # multiprocessing
+    run_sorters(sorter_list, recording_dict, working_folder, engine='multiprocessing')
+    
+    
     
     
     
 if __name__ == '__main__':
+    #~ test_run_sorters_with_list()
+    
     test_run_sorters()
