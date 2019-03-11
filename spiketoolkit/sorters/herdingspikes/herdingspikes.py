459115001f8d437057043f128457573e56f9d157
martinosorb <martino.sorbaro@ed.ac.uk>
2019-03-11 19:11:10 +0000

    proper dictionary of parameters now

--- spiketoolkit/sorters/herdingspikes/herdingspikes.py
+++ spiketoolkit/sorters/herdingspikes/herdingspikes.py
@@ -40,96 +40,57 @@ class HerdingspikesSorter(BaseSorter):
             shutil.rmtree(str(output_folder))
         os.makedirs(str(output_folder))
 
-        # # save prb file:
-        # probe_file = output_folder / 'probe.prb'
-        # se.saveProbeFile(recording, probe_file, format='spyking_circus')
-        #
-        # # save binary file
-        # raw_filename = output_folder / 'raw_signals.raw'
-        # traces = recording.getTraces()
-        # dtype = traces.dtype
-        # with raw_filename.open('wb') as f:
-        #     f.write(traces.T.tobytes())
-        #
-        # # initialize source and probe file
-        # hs_dataio = hs.DataIO(dirname=str(output_folder))
-        # nb_chan = recording.getNumChannels()
-        #
-        # hs_dataio.set_data_source(type='RawData', filenames=[str(raw_filename)],
-        #                           dtype=dtype.str,
-        #                           sample_rate=recording.getSamplingFrequency(),
-        #                           total_channel=nb_chan)
-        # hs_dataio.set_probe_file(str(probe_file))
-        # if self.debug:
-        #     print(hs_dataio)
-
-        inner_radius = 50
-        neighbor_radius = 50
-        noise_duration = 4
-        spike_peak_duration = 4
-
         # this should have its name changed
-        self.Probe = hs.probe.RecordingExtractor(
-            recording, inner_radius=inner_radius, neighbor_radius=neighbor_radius,
-            noise_duration=noise_duration, spike_peak_duration=spike_peak_duration)
+        self.Probe = hs.probe.RecordingExtractor(recording,
+                                                 **self.params['probe_params'])
 
     def _run(self, recording, output_folder):
-        # detection parameters
-        to_localize = True
-        cutout_start = 10
-        cutout_end = 34
-        threshold = 20
-        num_com_centers = 3
 
-        H = hs.HSDetection(self.Probe, to_localize, num_com_centers, cutout_start,
-                           cutout_end, threshold, maa=0, maxsl=13, minsl=2, ahpthr=5,
-                           out_file_name="HS2_detected", file_directory_name=output_folder,
-                           decay_filtering=False, save_all=False)
+        H = hs.HSDetection(self.Probe, file_directory_name=output_folder,
+                           **self.params['detection_params'])  # risky
 
         H.DetectFromRaw(load=True)
 
         C = hs.HSClustering(H)
-        C.ShapePCA(pca_ncomponents=2, pca_whiten=True)
-        C.CombinedClustering(alpha=6, bandwidth=6, bin_seeding=False, n_jobs=-1)
+        C.ShapePCA(**self.params['pca_params'])
+        C.CombinedClustering(**self.params['clustering_params'])
 
         sorted_file = str(output_folder / 'HS2_sorted.hdf5')
         C.SaveHDF5(sorted_file)
 
 
 HerdingspikesSorter._default_params = {
-    'fullchain_kargs': {
-        'duration': 300.,
-        'preprocessor': {
-            'highpass_freq': None,
-            'lowpass_freq': None,
-            'smooth_size': 0,
-            'chunksize': 1024,
-            'lostfront_chunksize': 128,
-            'signalpreprocessor_engine': 'numpy',
-            'common_ref_removal': False,
-        },
-        'peak_detector': {
-            'peakdetector_engine': 'numpy',
-            'peak_sign': '-',
-            'relative_threshold': 5.5,
-            'peak_span': 0.0002,
-        },
-        'noise_snippet': {
-            'nb_snippet': 300,
-        },
-        'extract_waveforms': {
-            'n_left': -45,
-            'n_right': 60,
-            'mode': 'rand',
-            'nb_max': 20000,
-            'align_waveform': False,
-        },
-        'clean_waveforms': {
-            'alien_value_threshold': 100.,
-        },
+    'probe_params': {
+        'inner_radius': 50,
+        'neighbor_radius': 50,
+        'noise_duration': 4,
+        'spike_peak_duration': 4
     },
-    'feat_method': 'peak_max',
-    'feat_kargs': {},
-    'clust_method': 'sawchaincut',
-    'clust_kargs': {'kde_bandwith': 1.},
+
+    'detection_params': {
+        'to_localize': True,
+        'cutout_start': 10,
+        'cutout_end': 34,
+        'threshold': 20,
+        'num_com_centers': 3,
+        'maa': 0,
+        'maxsl': 13,
+        'minsl': 2,
+        'ahpthr': 5,
+        'out_file_name': "HS2_detected",
+        'decay_filtering': False,
+        'save_all': False
+    },
+
+    'pca_params': {
+        'pca_ncomponents': 2,
+        'pca_whiten': True
+    },
+
+    'clustering_params': {
+        'alpha': 6.0,
+        'bandwidth': 6.0,
+        'bin_seeding': False,
+        'n_jobs': -1
+    }
 }
