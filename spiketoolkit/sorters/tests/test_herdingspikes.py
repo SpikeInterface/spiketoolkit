351428fec3682f17c2fd71163fadb0d8aba8c11c
Samuel Garcia <sam.garcia.die@gmail.com>
2019-03-18 16:30:41 +0100

    Add test for HS2

---
+++ spiketoolkit/sorters/tests/test_herdingspikes.py
@@ -0,0 +1,22 @@
+import unittest
+import pytest
+
+from spiketoolkit.sorters import HerdingspikesSorter
+from spiketoolkit.sorters.tests.common_tests import SorterCommonTestSuite
+
+import spikeextractors as se
+
+# This run several tests
+@pytest.mark.skipif(not HerdingspikesSorter.installed, reason='herdingspikes not installed')
+class HerdingspikesSorterCommonTestSuite(SorterCommonTestSuite, unittest.TestCase):
+    SorterClass = HerdingspikesSorter
+
+
+
+if __name__ == '__main__':
+    HerdingspikesSorterCommonTestSuite().test_on_toy()
+    #~ classHerdingspikesSorterCommonTestSuite().test_several_groups()
+    
+    
+    
+    
