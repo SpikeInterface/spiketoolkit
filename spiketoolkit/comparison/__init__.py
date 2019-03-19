253a3fae00191139ccb386c4cfb94cd0d9fe0eaa
Samuel Garcia <sam.garcia.die@gmail.com>
2019-03-19 16:13:51 +0100

    Start refactoring comparisontools.

--- spiketoolkit/comparison/__init__.py
+++ spiketoolkit/comparison/__init__.py
@@ -1,3 +1,5 @@
 from .sortingcomparison import SortingComparison, MappedSortingExtractor, compute_performance, confusion_matrix
 from .multisortingcomparison import MultiSortingComparison
 from .gathersortingcomparison import gather_sorting_comparison
+from .comparisontools import get_matching, get_counting, get_confusion
+
---
+++ spiketoolkit/comparison/tests/test_comparisontools.py
@@ -0,0 +1,25 @@
+import pytest
+
+from spiketoolkit.comparison import do_matching, do_counting, do_confusion
+
+import spikeextractors as se
+
+
+
+
+def test_do_matching():
+    pass
+    
+    
+def test_do_counting():
+    pass
+
+def test_do_confusion():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    test_do_matching()
+    test_do_counting()
