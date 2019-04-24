e869593e790a25fe86b15caadf8ecb9706bbcd84
colehurwitz <s1727843@raikonnen.inf.ed.ac.uk>
2019-04-24 19:25:47 +0100

    Preprocessing list

---
+++ spiketoolkit/preprocessing/preprocessinglist.py
@@ -0,0 +1,7 @@
+from .bandpass_filter import bandpass_filter, BandpassFilterRecording
+from .notch_filter import notch_filter, NotchFilterRecording
+from .whiten import whiten, WhitenRecording
+from .common_reference import common_reference, CommonReferenceRecording
+from .resample import resample, ResampledRecording
+from .rectify import rectify, RectifyRecording
+from .remove_artifacts import remove_artifacts, RemoveArtifactsRecording
