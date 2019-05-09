500502a3fcb8ea8e7ff6f0aec60f7e6b4680d892
Samuel Garcia <sam.garcia.die@gmail.com>
2019-05-09 13:13:20 +0200

    Make explicit loops when n_jobs=-1 to avoid pickle errors

--- spiketoolkit/comparison/comparisontools.py
+++ spiketoolkit/comparison/comparisontools.py
@@ -31,6 +31,17 @@ def compute_agreement_score(num_matches, num1, num2):
     return num_matches / denom
 
 
+def match_spikes(times1, all_times2, unit2_ids, delta_frames, event_counts1, event_counts2):
+    matching_event_counts = []
+    scores = []
+    for i2, u2 in enumerate(unit2_ids):
+        times2 = all_times2[i2]
+        num_matches = count_matching_events(times1, times2, delta=delta_frames)
+        matching_event_counts.append(num_matches)
+        scores.append(compute_agreement_score(num_matches, event_counts1, event_counts2[i2]))
+    return matching_event_counts, scores
+
+
 def do_matching(sorting1, sorting2, delta_frames, min_accuracy, n_jobs=-1):
     """
     This compute the matching between 2 sorters.
@@ -97,20 +108,16 @@ def do_matching(sorting1, sorting2, delta_frames, min_accuracy, n_jobs=-1):
         event_counts_2[u2] = len(times2)
 
     # Compute matching events
-    def match_spikes(times1, all_times2, unit2_ids, delta_frames, event_counts1, event_counts2):
-        matching_event_counts = []
-        scores = []
-        for i2, u2 in enumerate(unit2_ids):
-            times2 = all_times2[i2]
-            num_matches = count_matching_events(times1, times2, delta=delta_frames)
-            matching_event_counts.append(num_matches)
-            scores.append(compute_agreement_score(num_matches, event_counts1, event_counts2[i2]))
-        return matching_event_counts, scores
-
     s2_spiketrains = [sorting2.get_unit_spike_train(u2) for u2 in unit2_ids]
-    results = Parallel(n_jobs=n_jobs)(delayed(match_spikes)(sorting1.get_unit_spike_train(u1), s2_spiketrains,
-                                                            unit2_ids, delta_frames, event_counts1[i1],
-                                                            event_counts2) for i1, u1 in enumerate(unit1_ids))
+    args_list = ((sorting1.get_unit_spike_train(u1), s2_spiketrains, unit2_ids, delta_frames,
+                        event_counts1[i1], event_counts2) for i1, u1 in enumerate(unit1_ids))
+    if n_jobs == -1:
+        results = []
+        for args in args_list:
+            results.append(match_spikes(*args))
+    else:
+        results = Parallel(n_jobs=n_jobs)(delayed(match_spikes)(args_list))
+
     matching_event_counts = np.zeros((N1, N2)).astype(np.int64)
     scores = np.zeros((N1, N2))
     for i1, u1 in enumerate(unit1_ids):
