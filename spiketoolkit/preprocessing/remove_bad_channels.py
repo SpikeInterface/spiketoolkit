0148520f43f8125a6bb6d2b2207fae4495a05e0c
colehurwitz <colehurwitz@gmail.com>
2019-09-09 16:31:39 +0100

    auto --> None

--- spiketoolkit/preprocessing/remove_bad_channels.py
+++ spiketoolkit/preprocessing/remove_bad_channels.py
@@ -6,7 +6,7 @@ class RemoveBadChannelsRecording(RecordingExtractor):
     preprocessor_name = 'RemoveBadChannels'
     installed = True  # check at class level if installed or not
     _gui_params = [
-        {'name': 'bad_channel_ids', 'type': 'int_list', 'title': "List of bad channels or 'auto'"},
+        {'name': 'bad_channel_ids', 'type': 'int_list', 'value':None, 'default':None, 'title': "List of bad channels. If None, automatic removal will be done based on standard deviation."},
         {'name': 'bad_threshold', 'type': 'float', 'title': "Threshold in number of sd to remove channels ('auto')"},
         {'name': 'seconds', 'type': 'float', 'title': "Number of seconds to compute standard deviation ('auto)"},
         {'name': 'verbose', 'type': 'bool', 'title': "If True output is verbose"},
@@ -51,7 +51,7 @@ class RemoveBadChannelsRecording(RecordingExtractor):
                 if chan not in self._bad_channel_ids:
                     active_channels.append(chan)
             self._subrecording = SubRecordingExtractor(self._recording, channel_ids=active_channels)
-        elif self._bad_channel_ids == 'auto':
+        elif self._bad_channel_ids is None:
             start_frame = self._recording.get_num_frames() // 2
             end_frame = int(start_frame + self._seconds * self._recording.get_sampling_frequency())
             traces = self._recording.get_traces(start_frame=start_frame, end_frame=end_frame)
@@ -69,7 +69,7 @@ class RemoveBadChannelsRecording(RecordingExtractor):
         self.active_channels = self._subrecording.get_channel_ids()
 
 
-def remove_bad_channels(recording, bad_channel_ids, bad_threshold=2, seconds=10, verbose=False):
+def remove_bad_channels(recording, bad_channel_ids=None, bad_threshold=2, seconds=10, verbose=False):
     '''
     Remove bad channels from the recording extractor.
 
@@ -77,12 +77,12 @@ def remove_bad_channels(recording, bad_channel_ids, bad_threshold=2, seconds=10,
     ----------
     recording: RecordingExtractor
         The recording extractor object
-    bad_channel_ids: list or 'auto'
-        List of bad channel ids (int) or 'auto' for automatic removal based on standard deviation
+    bad_channel_ids: list
+        List of bad channel ids (int). If None, automatic removal will be done based on standard deviation.
     bad_threshold: float
-        If 'auto' is used, the threshold for the standard deviation over which channels are removed
+        If automatic is used, the threshold for the standard deviation over which channels are removed
     seconds: float
-        If 'auto' is used, the number of seconds used to compute standard deviations
+        If automatic is used, the number of seconds used to compute standard deviations
     verbose: bool
         If True, output is verbose
 
