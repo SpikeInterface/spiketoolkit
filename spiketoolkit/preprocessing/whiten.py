b44bcf0b8cf4fc0b207ea21daaa75ce4c935fed0
Cole Hurwitz <colehurwitz@gmail.com>
2019-09-16 21:35:23 +0100

    Update whiten.py

--- spiketoolkit/preprocessing/whiten.py
+++ spiketoolkit/preprocessing/whiten.py
@@ -7,14 +7,14 @@ class WhitenRecording(FilterRecording):
     preprocessor_name = 'Whiten'
     installed = True  # check at class level if installed or not
     preprocessor_gui_params = [
-        {'name': 'chunk_size', 'type': 'int', 'value': 1000, 'default': 1000, 'title':
+        {'name': 'chunk_size', 'type': 'int', 'value': 30000, 'default': 30000, 'title':
             "Chunk size for the filter."},
         {'name': 'cache', 'type': 'bool', 'value': False, 'default': False, 'title':
             "If True filtered traces are computed and cached"},
     ]
     installation_mesg = ""  # err
 
-    def __init__(self, recording, chunk_size=1000, cache=False):
+    def __init__(self, recording, chunk_size=30000, cache=False):
         self._recording = recording
         self._whitening_matrix = self._compute_whitening_matrix()
         FilterRecording.__init__(self, recording=recording, chunk_size=chunk_size, cache=cache)
@@ -42,7 +42,7 @@ class WhitenRecording(FilterRecording):
         return chunk2
 
 
-def whiten(recording, chunk_size=1000, cache=False):
+def whiten(recording, chunk_size=30000, cache=False):
     '''
     Whitens the recording extractor traces.
 
