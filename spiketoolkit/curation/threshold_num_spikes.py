79c710e74580f8fd74aade24c3d6e288f89d1808
colehurwitz <colehurwitz@gmail.com>
2019-11-07 17:14:33 +0000

    removed sampling frequency from curation num spikes

--- spiketoolkit/curation/threshold_num_spikes.py
+++ spiketoolkit/curation/threshold_num_spikes.py
@@ -2,6 +2,8 @@ from .thresholdcurator import ThresholdCurator
 import spiketoolkit as st
 
 
+#No sampling frequency needed, special curator that does NOT use the metric calculator.
+
 class ThresholdNumSpikes(ThresholdCurator):
     curator_name = 'ThresholdNumSpikes'
     installed = True  # check at class level if installed or not
@@ -16,31 +18,20 @@ class ThresholdNumSpikes(ThresholdCurator):
     ]
     installation_mesg = ""  # err
 
-    def __init__(self, sorting, threshold=100, threshold_sign='less', sampling_frequency=None, metric_calculator=None):
+    def __init__(self, sorting, threshold=100, threshold_sign='less', metric_calculator=None):
         metric_name = 'num_spikes'
-        if sampling_frequency is None and sorting.get_sampling_frequency() is None:
-            raise ValueError("Please pass in a sampling frequency (your SortingExtractor does not have one specified)")
-        elif sampling_frequency is None:
-            self._sampling_frequency = sorting.get_sampling_frequency()
-        else:
-            self._sampling_frequency = sampling_frequency
-        if metric_calculator is None:
-            self._metric_calculator = st.validation.MetricCalculator(sorting,
-                                                                     sampling_frequency=self._sampling_frequency,
-                                                                     unit_ids=None, epoch_tuples=None, epoch_names=None)
-            self._metric_calculator.compute_num_spikes()
-        else:
+        if metric_calculator is not None:
             self._metric_calculator = metric_calculator
             if metric_name not in self._metric_calculator.get_metrics_dict().keys():
                 self._metric_calculator.compute_num_spikes()
-        num_spikes_epoch = self._metric_calculator.get_metrics_dict()[metric_name][0]
-
+            num_spikes_epoch = self._metric_calculator.get_metrics_dict()[metric_name][0]
+        else:
+            num_spikes_epoch = [len(sorting.get_unit_spike_train(unit_id)) for unit_id in sorting.get_unit_ids()]
         ThresholdCurator.__init__(self, sorting=sorting, metrics_epoch=num_spikes_epoch)
         self.threshold_sorting(threshold=threshold, threshold_sign=threshold_sign)
 
 
-def threshold_num_spikes(sorting, threshold=100, threshold_sign='less', sampling_frequency=None,
-                         metric_calculator=None):
+def threshold_num_spikes(sorting, threshold=100, threshold_sign='less', metric_calculator=None):
     '''
     Excludes units based on number of spikes.
 
@@ -55,8 +46,6 @@ def threshold_num_spikes(sorting, threshold=100, threshold_sign='less', sampling
         If 'less_or_equal', will remove any units with metric scores less than or equal to the given threshold.
         If 'greater', will remove any units with metric scores greater than the given threshold.
         If 'greater_or_equal', will remove any units with metric scores greater than or equal to the given threshold.
-    sampling_frequency: float
-        The sampling frequency of recording
     metric_calculator: MetricCalculator
         A metric calculator can be passed in with cached metrics.
     Returns
@@ -69,6 +58,5 @@ def threshold_num_spikes(sorting, threshold=100, threshold_sign='less', sampling
         sorting=sorting,
         threshold=threshold,
         threshold_sign=threshold_sign,
-        sampling_frequency=sampling_frequency,
         metric_calculator=metric_calculator
     )
