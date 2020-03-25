707efb6bc711ae4c7290eb1a38441d0f7e8309ef
Alessio Buccino <alejoe9187@gmail.com>
2020-03-25 22:44:56 +0100

    transform traces accept list of scalar and offsets for each electrode

--- spiketoolkit/preprocessing/transform_traces.py
+++ spiketoolkit/preprocessing/transform_traces.py
@@ -6,8 +6,10 @@ class TransformTracesRecording(RecordingExtractor):
     preprocessor_name = 'TransformTraces'
     installed = True  # check at class level if installed or not
     preprocessor_gui_params = [
-        {'name': 'scalar', 'type': 'float', 'title': "Scalar for the traces of the recording extractor"},
-        {'name': 'offset', 'type': 'float', 'title': "Offset for the traces of the recording extractor"},
+        {'name': 'scalar', 'type': 'float or array', 'title': "Scalar for all traces or each channel "
+                                                              "of the recording extractor"},
+        {'name': 'offset', 'type': 'float or array', 'title': "Offset for all traces or each channel"
+                                                              "of the recording extractor"},
     ]
     installation_mesg = ""  # err
 
@@ -21,7 +23,6 @@ class TransformTracesRecording(RecordingExtractor):
         self.copy_channel_properties(recording=self._recording)
 
         self._kwargs = {'recording': recording.make_serialized_dict(), 'scalar': scalar, 'offset': offset}
-
 
     def get_sampling_frequency(self):
         return self._recording.get_sampling_frequency()
@@ -40,7 +41,25 @@ class TransformTracesRecording(RecordingExtractor):
         if channel_ids is None:
             channel_ids = self.get_channel_ids()
         traces = self._recording.get_traces(channel_ids=channel_ids, start_frame=start_frame, end_frame=end_frame)
-        return traces*self._scalar + self._offset
+        if isinstance(self._scalar, (int, float, np.integer, np.float)):
+            traces = traces*self._scalar
+        else:
+            if len(self._scalar) == len(channel_ids):
+                scalar = self._scalar
+            else:
+                channel_idxs = np.array([self._recording.get_channel_ids().index(ch) for ch in channel_ids])
+                scalar = self._scalar[channel_idxs]
+            traces = np.array([t * s for (t, s) in zip(traces, scalar)])
+        if isinstance(self._offset, (int, float, np.integer, np.float)):
+            traces = traces + self._offset
+        else:
+            if len(self._offset) == len(channel_ids):
+                offset = self._offset
+            else:
+                channel_idxs = np.array([self._recording.get_channel_ids().index(ch) for ch in channel_ids])
+                offset = self._offset[channel_idxs]
+            traces = np.array([t + o for (t, o) in zip(traces, offset)])
+        return traces
 
 
 def transform_traces(recording, scalar=1, offset=0):
