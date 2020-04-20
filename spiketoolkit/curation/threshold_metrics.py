8f681f2869b4a969a7b1ca7564b093c553a6ad80
colehurwitz <colehurwitz@gmail.com>
2020-04-20 16:46:10 -0700

    update threshold metrics

--- spiketoolkit/curation/threshold_metrics.py
+++ spiketoolkit/curation/threshold_metrics.py
@@ -51,7 +51,7 @@ def threshold_num_spikes(
     params_dict = update_all_param_dicts_with_kwargs(kwargs)
 
     md = MetricData(sorting=sorting, sampling_frequency=sampling_frequency, recording=None, apply_filter=False,
-                    freq_min=300.0, freq_max=6000.0, unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_min=300.0, freq_max=6000.0, unit_ids=None, verbose=params_dict['verbose'])
 
     ns = NumSpikes(metric_data=md)
     threshold_sorting = ns.threshold_metric(threshold, threshold_sign, **kwargs)
@@ -62,6 +62,7 @@ def threshold_firing_rates(
         sorting,
         threshold,
         threshold_sign,
+        duration_in_frames,
         sampling_frequency=None,
         **kwargs
 ):
@@ -79,6 +80,8 @@ def threshold_firing_rates(
         If 'less_or_equal', will threshold any metric less than or equal to the given threshold
         If 'greater', will threshold any metric greater than the given threshold
         If 'greater_or_equal', will threshold any metric greater than or equal to the given threshold
+    duration_in_frames: int
+        Length of recording (in frames).
     sampling_frequency:
         The sampling frequency of the result. If None, will check to see if sampling frequency is in sorting extractor
     **kwargs: keyword arguments
@@ -95,7 +98,8 @@ def threshold_firing_rates(
     params_dict = update_all_param_dicts_with_kwargs(kwargs)
 
     md = MetricData(sorting=sorting, sampling_frequency=sampling_frequency, recording=None, apply_filter=False,
-                    freq_min=300.0, freq_max=6000.0, unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=duration_in_frames, freq_min=300.0, freq_max=6000.0, unit_ids=None, 
+                    verbose=params_dict['verbose'])
 
     fr = FiringRate(metric_data=md)
     threshold_sorting = fr.threshold_metric(threshold, threshold_sign, **kwargs)
@@ -106,6 +110,7 @@ def threshold_presence_ratios(
         sorting,
         threshold,
         threshold_sign,
+        duration_in_frames,
         sampling_frequency=None,
         **kwargs
 ):
@@ -123,6 +128,8 @@ def threshold_presence_ratios(
         If 'less_or_equal', will threshold any metric less than or equal to the given threshold
         If 'greater', will threshold any metric greater than the given threshold
         If 'greater_or_equal', will threshold any metric greater than or equal to the given threshold
+    duration_in_frames: int
+        Length of recording (in frames).
     sampling_frequency:
         The sampling frequency of the result. If None, will check to see if sampling frequency is in sorting extractor
     **kwargs: keyword arguments
@@ -139,7 +146,8 @@ def threshold_presence_ratios(
     params_dict = update_all_param_dicts_with_kwargs(kwargs)
 
     md = MetricData(sorting=sorting, sampling_frequency=sampling_frequency, recording=None, apply_filter=False,
-                    freq_min=300.0, freq_max=6000.0, unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=duration_in_frames, freq_min=300.0, freq_max=6000.0, unit_ids=None, 
+                    verbose=params_dict['verbose'])
 
     pr = PresenceRatio(metric_data=md)
     threshold_sorting = pr.threshold_metric(threshold, threshold_sign, **kwargs)
@@ -150,6 +158,7 @@ def threshold_isi_violations(
         sorting,
         threshold,
         threshold_sign,
+        duration_in_frames,
         isi_threshold=ISIViolation.params['isi_threshold'],
         min_isi=ISIViolation.params['min_isi'],
         sampling_frequency=None,
@@ -169,6 +178,8 @@ def threshold_isi_violations(
         If 'less_or_equal', will threshold any metric less than or equal to the given threshold
         If 'greater', will threshold any metric greater than the given threshold
         If 'greater_or_equal', will threshold any metric greater than or equal to the given threshold
+    duration_in_frames: int
+        Length of recording (in frames).
     isi_threshold: float
         The isi threshold for calculating isi violations.
     min_isi: float
@@ -189,7 +200,8 @@ def threshold_isi_violations(
     params_dict = update_all_param_dicts_with_kwargs(kwargs)
 
     md = MetricData(sorting=sorting, sampling_frequency=sampling_frequency, recording=None, apply_filter=False,
-                    freq_min=300.0, freq_max=6000.0, unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=duration_in_frames, freq_min=300.0, freq_max=6000.0, unit_ids=None, 
+                    verbose=params_dict['verbose'])
 
     iv = ISIViolation(metric_data=md)
     threshold_sorting = iv.threshold_metric(threshold, threshold_sign, isi_threshold, min_isi, **kwargs)
@@ -252,7 +264,7 @@ def threshold_amplitude_cutoffs(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
     md.compute_amplitudes(**kwargs)
 
     ac = AmplitudeCutoff(metric_data=md)
@@ -353,7 +365,7 @@ def threshold_snrs(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     snr = SNR(metric_data=md)
     threshold_sorting = snr.threshold_metric(threshold, threshold_sign, snr_mode, snr_noise_duration,
@@ -442,7 +454,7 @@ def threshold_silhouette_scores(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     md.compute_pca_scores(**kwargs)
 
@@ -536,7 +548,7 @@ def threshold_d_primes(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     md.compute_pca_scores(**kwargs)
 
@@ -630,7 +642,7 @@ def threshold_l_ratios(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     md.compute_pca_scores(**kwargs)
 
@@ -724,7 +736,7 @@ def threshold_isolation_distances(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     md.compute_pca_scores(**kwargs)
 
@@ -827,7 +839,7 @@ def threshold_nn_metrics(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     md.compute_pca_scores(**kwargs)
 
@@ -924,7 +936,7 @@ def threshold_drift_metrics(
 
     md = MetricData(sorting=sorting, sampling_frequency=recording.get_sampling_frequency(), recording=recording,
                     apply_filter=params_dict["apply_filter"], freq_min=params_dict["freq_min"],
-                    freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
+                    duration_in_frames=None, freq_max=params_dict["freq_max"], unit_ids=None, verbose=params_dict['verbose'])
 
     md.compute_pca_scores(**kwargs)
 
