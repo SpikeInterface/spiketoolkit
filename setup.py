020655ec4e7e1d242ecf300a3b6fbc45c8f5b507
Alessio Buccino <alejoe9187@gmail.com>
2020-08-13 18:37:25 +0200

    incr version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'numpy',
         'spikeextractors',
         'spikesorters',
-        'spikemetrics>=0.2.1',
+        'spikemetrics>=0.2.2',
         'spikefeatures',
         'scikit-learn',
         'scipy',
