0b1deea724f742797181bb4fe57270fdd84951c1
Fernando J. Chaure <ferchaure@users.noreply.github.com>
2020-12-14 10:42:25 -0300

    always copy trace in black_saturation

--- spiketoolkit/preprocessing/blank_saturation.py
+++ spiketoolkit/preprocessing/blank_saturation.py
@@ -46,8 +46,7 @@ class BlankSaturationRecording(BasePreprocessorRecordingExtractor):
         traces = self._recording.get_traces(channel_ids=channel_ids,
                                             start_frame=start_frame,
                                             end_frame=end_frame)
-        if isinstance(traces, np.memmap):
-            traces = traces.copy()
+        traces = traces.copy()
         if self._lower:
             traces[traces <= self._threshold] = self._median
         else:
