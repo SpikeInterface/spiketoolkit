2c54cd8c718ae11b8a341c97a8b6fa04363b483b
Alessio Buccino <alejoe9187@gmail.com>
2021-07-27 16:01:42 +0200

    incr si dependencies

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     package_data={},
     install_requires=[
         'numpy',
-        'spikeextractors>=0.9.4',
+        'spikeextractors>=0.9.7',
         'spikemetrics>=0.2.4',
         'spikefeatures',
         'scikit-learn',
